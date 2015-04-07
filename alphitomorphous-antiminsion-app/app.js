71a4712e4e7d07e90a85c861e14d70a18a562e52
Brett Levenson <bhlevenson@gmail.com>
2015-04-07 15:55:46 -0700

    Added user authentication logic and a few views

--- alphitomorphous-antiminsion-app/app.js
+++ alphitomorphous-antiminsion-app/app.js
@@ -2,13 +2,20 @@ var express = require('express');
 var bodyParser = require('body-parser'),
 	ejs = require('ejs'),
 	methodOverride = require('method-override'),
-	pg = require("pg");
+	pg = require("pg"),
+	session = require("express-session");
 
 
 var app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+  secret: 'super secret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // Refactor connection and query code
 var db = require("./models");
@@ -86,6 +93,50 @@ app.get('/contact', function(req,res) {
   res.render('site/contact');
 });
 
+
+// User routes
+
+app.get("/signup", function (req, res) {
+  res.render('users/new.ejs');
+});
+
+// where the user submits the sign-up form
+app.post("/users", function (req, res) {
+
+  // grab the user from the params
+  var user = req.body.user;
+
+  // create the new user
+  db.User
+  		.createSecure(user.email, user.password)
+    	.then(function(){
+        	res.redirect('/');
+      	});
+});
+
+app.get("/login", function(req, res) {
+	res.render("users/login");
+});
+
+app.post("/login", function (req, res) {
+  var user = req.body.user;
+
+  db.User
+    .authenticate(user.email, user.password)
+    .then(function (user) {
+          res.redirect("/users/" + user.id);
+    });
+});
+
+// Show user
+app.get("/users/:id", function(req, res) {
+	db.User.find(req.params.id)
+		.then(function(dbUser) {
+			res.render("users/user.ejs", { ejsUser: dbUser })
+		})	
+})
+
+// Sync route (only used by going to /sync - don't use unless you have a good reason)
 app.get('/sync', function(req, res) {
 	db.sequelize.sync().then(function() {
 		res.send("Sequelize Synchronization is Complete!");
